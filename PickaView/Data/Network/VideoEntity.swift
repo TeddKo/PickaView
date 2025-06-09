ce15927dc7d5d388e5c28b5439a5f5b521607895
kdn0325 <dn10003@gmail.com>
2025-06-09 14:51:21 +0900

    feat: Define response models for Pixabay API

--- PickaView/Data/Network/VideoEntity.swift
+++ PickaView/Data/Network/VideoEntity.swift
@@ -5,6 +5,24 @@
 //  Created by Ko Minhyuk on 6/5/25.
 //
 
+// 응답 모델 정의
+
 import Foundation
 
+struct PixabayResponse: Decodable {
+    let hits: [PixabayVideo]
+}
 
+struct PixabayVideo: Decodable {
+    let id: Int
+    let user: String
+    let user_id: Int
+    let userImageURL: String
+    let views: Int
+    let comments: Int
+    let videos: [String: VideoFile]
+}
+
+struct VideoFile: Decodable {
+    let url: String
+}
