8cf94992dfee13fc242193ba1e0a2dbe49300cb7
kdn0325 <dn10003@gmail.com>
2025-06-10 00:24:26 +0900

    feat: Update Pixabay response models to include additional fields and improve structure

--- PickaView/Data/Network/VideoEntity.swift
+++ PickaView/Data/Network/VideoEntity.swift
@@ -10,19 +10,44 @@
 import Foundation
 
 struct PixabayResponse: Decodable {
+    let total: Int
+    let totalHits: Int
     let hits: [PixabayVideo]
 }
 
 struct PixabayVideo: Decodable {
     let id: Int
-    let user: String
-    let user_id: Int
-    let userImageURL: String
+    let pageURL: String
+    let type: String
+    let tags: String
+    let duration: Int
+    let videos: VideoVariants
     let views: Int
+    let downloads: Int
+    let likes: Int
     let comments: Int
-    let videos: [String: VideoFile]
+    let userId: Int
+    let user: String
+    let userImageURL: String
+    let noAiTraining: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id, pageURL, type, tags, duration, videos, views, downloads, likes, comments, user, userImageURL, noAiTraining
+        case userId = "user_id"
+    }
+}
+
+struct VideoVariants: Decodable {
+    let large: VideoFile
+    let medium: VideoFile
+    let small: VideoFile
+    let tiny: VideoFile
 }
 
 struct VideoFile: Decodable {
     let url: String
+    let width: Int
+    let height: Int
+    let size: Int
+    let thumbnail: String
 }
