00b3b5325d2e933e0dc7abcce31ef37db681959a
Tedd <tddy.ko@kakao.com>
2025-06-11 23:16:13 +0900

    fix: 주석 수정

--- PickaView/Common/Components/Extension.swift
+++ PickaView/Common/Components/Extension.swift
@@ -34,20 +34,19 @@ extension UIImageView {
 
 
 extension UIView {
-    /**
-     주어진 뷰를 컨테이너 뷰로 감싸 좌우 여백을 적용하고 `stackView`에 추가하는 래퍼(Wrapper) 함수.
-     - Parameters:
-     - stackView: container와 view를 담을 스택.
-     - view: 여백을 적용할 대상 뷰.
-     - leftPadding: 왼쪽에 적용할 여백 크기.
-     - rightPadding: 오른쪽에 적용할 여백.
-     **/
+    /// 특정 방향의 여백을 채우기 위한 UIView의 확장 래퍼함수.
+    /// - Parameters:
+    ///   - stackView: container와 view를 담을 스택.
+    ///   - topPadding: 위쪽 방향에 적용할 여백.
+    ///   - leftPadding: 왼쪽 방향에 적용할 여백.
+    ///   - rightPadding: 오른쪽 방향에 적용할 여백.
+    ///   - bottomPadding: 아래쪽 방향에 적용할 여백.
     func wrappedPaddingContainer(
         stackView: UIStackView,
-        topPadding: CGFloat,
-        leftPadding: CGFloat,
-        rightPadding: CGFloat,
-        bottomPadding: CGFloat
+        topPadding: CGFloat? = nil,
+        leftPadding: CGFloat? = nil,
+        rightPadding: CGFloat? = nil,
+        bottomPadding: CGFloat? = nil
     ) {
         let containerView = UIView()
         containerView.translatesAutoresizingMaskIntoConstraints = false
@@ -58,20 +57,17 @@ extension UIView {
         containerView.addSubview(self)
         
         NSLayoutConstraint.activate([
-            self.topAnchor.constraint(equalTo: containerView.topAnchor, constant: topPadding),
-            self.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: leftPadding),
-            self.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -rightPadding),
-            self.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -bottomPadding),
+            self.topAnchor.constraint(equalTo: containerView.topAnchor, constant: topPadding ?? 0),
+            self.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: leftPadding ?? 0),
+            self.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -(rightPadding ?? 0)),
+            self.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -(bottomPadding ?? 0)),
         ])
     }
-
-    /**
-     주어진 뷰를 컨테이너 뷰로 감싸 좌우 여백을 적용하고 `stackView`에 추가하는 래퍼(Wrapper) 함수.
-     - Parameters:
-     - stackView: container와 view를 담을 스택.
-     - view: 여백을 적용할 대상 뷰.
-     - allPadding: 왼쪽에 적용할 여백.
-     **/
+    
+    /// 모든방향의 여백을 채우기 위한 UIView의 확장 래퍼함수.
+    /// - Parameters:
+    ///   - stackView: container와 view를 담을 스택.
+    ///   - allPadding: 모든 방향에 적용할 여백.
     func wrappedPaddingContainer(
         stackView: UIStackView,
         allPadding: CGFloat
@@ -85,13 +81,11 @@ extension UIView {
         )
     }
 
-    /**
-     주어진 뷰를 컨테이너 뷰로 감싸 좌우 여백을 적용하고 `stackView`에 추가하는 래퍼(Wrapper) 함수.
-     - Parameters:
-     - stackView: container와 view를 담을 스택.
-     - view: 여백을 적용할 대상 뷰.
-     - horizontalPadding: 양쪽에 가로방향에 적용할 여백.
-     **/
+    
+    /// 가로방향의 여백을 채우기 위한 UIView의 확장 래퍼함수.
+    /// - Parameters:
+    ///   - stackView: container와 view를 담을 스택.
+    ///   - horizontalPadding: 가로방향에 적용할 여백.
     func wrappedPaddingContainer(
         stackView: UIStackView,
         horizontalPadding: CGFloat
@@ -105,13 +99,11 @@ extension UIView {
         )
     }
 
-    /**
-     주어진 뷰를 컨테이너 뷰로 감싸 좌우 여백을 적용하고 `stackView`에 추가하는 래퍼(Wrapper) 함수.
-     - Parameters:
-     - stackView: container와 view를 담을 스택.
-     - view: 여백을 적용할 대상 뷰.
-     - verticalPadding: 양쪽 세로방향에 적용할 여백.
-     **/
+    
+    /// 세로방향의 여백을 채우기 위한 UIView의 확장 래퍼함수.
+    /// - Parameters:
+    ///   - stackView: container와 view를 담을 스택.
+    ///   - verticalPadding: 세로방향에 적용할 여백.
     func wrappedPaddingContainer(
         stackView: UIStackView,
         verticalPadding: CGFloat
