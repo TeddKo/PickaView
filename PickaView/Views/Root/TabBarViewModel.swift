70aa9eedb930e245dc134c2dd6109e1f1ff612e1
jihyeonjjang <jihyeon9975@gmail.com>
2025-06-13 02:43:23 +0900

    refactor: 탭별 ViewController의 스토리보드 인스턴스화 코드 개선
    - Home, Like, MyPage 스토리보드 객체와 VC 인스턴스 생성 로직 분리
    - ViewModel 주입 부분은 추후 작업을 위해 주석 처리 상태로 유지

--- PickaView/Views/Root/TabBarViewModel.swift
+++ PickaView/Views/Root/TabBarViewModel.swift
@@ -18,20 +18,20 @@ final class TabBarViewModel {
 
     func makeTabViewControllers() -> [UIViewController] {
         let homeVM = HomeViewModel(coreDataManager: coreDataManager, pixabayVideoService: pixabayVideoService)
-        var storyboard = UIStoryboard(name: "Home", bundle: nil)
-        let homeVC = storyboard.instantiateViewController(withIdentifier: String(describing: HomeViewController.self)) as! HomeViewController
+        let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
+        let homeVC = homeStoryboard.instantiateViewController(withIdentifier: String(describing: HomeViewController.self)) as! HomeViewController
         homeVC.viewModel = homeVM
         homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)
 
 //        let likeVM = LikeViewModel(coreDataManager: coreDataManager)
-        storyboard = UIStoryboard(name: "Like", bundle: nil)
-        let likeVC = storyboard.instantiateViewController(withIdentifier: String(describing: LikeViewController.self)) as! LikeViewController
+        let likeStoryboard = UIStoryboard(name: "Like", bundle: nil)
+        let likeVC = likeStoryboard.instantiateViewController(withIdentifier: String(describing: LikeViewController.self)) as! LikeViewController
 //        likeVC.viewModel = likeVM
         likeVC.tabBarItem = UITabBarItem(title: "Like", image: UIImage(systemName: "heart.fill"), tag: 1)
 
 //        let myPageVM = MyPageViewModel(coreDataManager: coreDataManager)
-        storyboard = UIStoryboard(name: "MyPage", bundle: nil)
-        let myPageVC = storyboard.instantiateViewController(withIdentifier: String(describing: MyPageViewController.self)) as! MyPageViewController
+        let myPageStoryboard = UIStoryboard(name: "MyPage", bundle: nil)
+        let myPageVC = myPageStoryboard.instantiateViewController(withIdentifier: String(describing: MyPageViewController.self)) as! MyPageViewController
 //        myPageVC.viewModel = myPageVM
         myPageVC.tabBarItem = UITabBarItem(title: "MyPage", image: UIImage(systemName: "ellipsis"), tag: 2)
         
