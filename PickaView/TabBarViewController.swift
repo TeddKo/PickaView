85ae403cc31c1f5f1aae9aa1a22d4e948d21c4de
jihyeonjjang <jihyeon9975@gmail.com>
2025-06-13 01:16:07 +0900

    refactor: TabBarViewModel 내 ViewController 인스턴스화를 스토리보드 방식으로 전환
    - Home, Like, MyPage 탭의 ViewController를 코드 생성 → 스토리보드 기반으로 변경 (스토리보드를 유지하기 위함)
    - 각 ViewModel에 필요한 의존성(CoreDataManager, VideoService) 명시적으로 주입

--- PickaView/TabBarViewController.swift
+++ PickaView/TabBarViewController.swift
@@ -8,20 +8,11 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-    private let viewModel: TabBarViewModel
-    
-    init(viewModel: TabBarViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private let viewModel = TabBarViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         ThemeManager.shared.applyTheme()
         print("initial")
         
--- PickaView/Views/Root/TabBarViewModel.swift
+++ PickaView/Views/Root/TabBarViewModel.swift
@@ -8,29 +8,37 @@
 import UIKit
 
 final class TabBarViewModel {
-    private let coreDataManager: CoreDataManager
+    let coreDataManager: CoreDataManager
+    let pixabayVideoService: PixabayVideoService
 
-    init(coreDataManager: CoreDataManager) {
-        self.coreDataManager = coreDataManager
+    init() {
+        self.coreDataManager = CoreDataManager()
+        self.pixabayVideoService = PixabayVideoService()
     }
 
     func makeTabViewControllers() -> [UIViewController] {
-//        let homeVM = HomeViewModel(coreDataManager: coreDataManager)
-//        let homeVC = HomeViewController(viewModel: homeVM)
-//        homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)
-//
-//        let likeVM = LikeViewModel(coreDataManager: coreDataManager)
-//        let likeVC = LikeViewController(viewModel: likeVM)
-//        likeVC.tabBarItem = UITabBarItem(title: "Like", image: UIImage(systemName: "heart.fill"), tag: 1)
-//
-//        let myPageVM = MyPageViewModel(coreDataManager: coreDataManager)
-//        let myPageVC = MyPageViewController(viewModel: myPageVM)
-//        myPageVC.tabBarItem = UITabBarItem(title: "MyPage", image: UIImage(systemName: "ellipsis"), tag: 2)
+        let homeVM = HomeViewModel(coreDataManager: coreDataManager, pixabayVideoService: pixabayVideoService)
+        var storyboard = UIStoryboard(name: "Home", bundle: nil)
+        let homeVC = storyboard.instantiateViewController(withIdentifier: String(describing: HomeViewController.self)) as! HomeViewController
+        homeVC.viewModel = homeVM
+        homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)
 
+//        let likeVM = LikeViewModel(coreDataManager: coreDataManager)
+        storyboard = UIStoryboard(name: "Like", bundle: nil)
+        let likeVC = storyboard.instantiateViewController(withIdentifier: String(describing: LikeViewController.self)) as! LikeViewController
+//        likeVC.viewModel = likeVM
+        likeVC.tabBarItem = UITabBarItem(title: "Like", image: UIImage(systemName: "heart.fill"), tag: 1)
+
+//        let myPageVM = MyPageViewModel(coreDataManager: coreDataManager)
+        storyboard = UIStoryboard(name: "MyPage", bundle: nil)
+        let myPageVC = storyboard.instantiateViewController(withIdentifier: String(describing: MyPageViewController.self)) as! MyPageViewController
+//        myPageVC.viewModel = myPageVM
+        myPageVC.tabBarItem = UITabBarItem(title: "MyPage", image: UIImage(systemName: "ellipsis"), tag: 2)
+        
         return [
-//            UINavigationController(rootViewController: homeVC),
-//            UINavigationController(rootViewController: likeVC),
-//            UINavigationController(rootViewController: myPageVC)
+            UINavigationController(rootViewController: homeVC),
+            UINavigationController(rootViewController: likeVC),
+            UINavigationController(rootViewController: myPageVC)
         ]
     }
 }
