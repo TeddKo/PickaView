a0eb032caebaaf31df53f98e181c2abc0c57d5ec
jihyeonjjang <jihyeon9975@gmail.com>
2025-06-12 09:44:13 +0900

    feat: 각 ViewModel마다 의존성 주입

--- PickaView/MainViewController.swift
+++ PickaView/MainViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class MainViewController: UITabBarController {
+    
+    let coreDataManager = CoreDataManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         ThemeManager.shared.applyTheme()
         print("initial")
+        
+        if let viewControllers = viewControllers {
+            for vc in viewControllers {
+                if let nav = vc as? UINavigationController, let topVC = nav.topViewController {
+                    injectViewModel(to: topVC)
+                } else {
+                    injectViewModel(to: vc)
+                }
+            }
+        }
+    }
+    
+    private func injectViewModel(to viewController: UIViewController) {
+//        switch viewController {
+//        case let vc as HomeViewController:
+//            vc.viewModel = HomeViewModel(coreDataManager: coreDataManager)
+//        case let vc as LikeViewController:
+//            vc.viewModel = LikeViewModel(coreDataManager: coreDataManager)
+//        case let vc as MyPageViewController:
+//            vc.viewModel = MyPageViewController(coreDataManager: coreDataManager)
+//        case let vc as PlayerViewController:
+//            vc.viewModel = PlayerViewController(coreDataManager: coreDataManager)
+//        }
     }
 }
